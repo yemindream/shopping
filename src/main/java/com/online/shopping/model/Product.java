70500e612e39201362c23b61803ccd6a9ed481e7
rongyu.yan <rongyu.yan@tcl.com>
2020-06-05 14:58:22 +0800

    添加商品列表页面

--- src/main/java/com/online/shopping/model/Product.java
+++ src/main/java/com/online/shopping/model/Product.java
@@ -24,4 +24,59 @@ public class Product {
 
     private byte[] image;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Integer seller) {
+        this.seller = seller;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
 }
--- src/main/java/com/online/shopping/model/ProductPage.java
+++ src/main/java/com/online/shopping/model/ProductPage.java
@@ -2,8 +2,15 @@ package com.online.shopping.model;
 
 import com.online.shopping.service.IShoppingService;
 import com.online.shopping.ui.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,18 +18,62 @@ import org.springframework.beans.factory.annotation.Autowired;
  * Created by yanry on 2020/6/5.
  */
 @Route(value = "product", layout = MainLayout.class)
-public class ProductPage extends VerticalLayout {
+public class ProductPage extends HorizontalLayout implements HasUrlParameter<Integer> {
     private Grid<Product> grid = new Grid<>(Product.class);
     @Autowired
     private IShoppingService shoppingService;
 
     public ProductPage() {
+        ProductForm productForm = new ProductForm();
+        productForm.setProduct(null);
         grid.setColumns("type", "price", "quantity", "description");
+        grid.asSingleSelect().addValueChangeListener(event -> {
+            productForm.setProduct(grid.asSingleSelect().getValue());
+        });
+        Button btnAdd = new Button("Add", event -> {
+            grid.asSingleSelect().clear();
+            productForm.setProduct(new Product());
+        });
 
-        add(grid);
+        add(new VerticalLayout(btnAdd, grid), productForm);
     }
 
     private void updateList() {
-//        grid.setItems(shoppingService.);
+        // todo
+    }
+
+    @Override
+    public void setParameter(BeforeEvent event, Integer parameter) {
+        updateList();
+    }
+
+    private class ProductForm extends FormLayout {
+        private TextField type = new TextField("Type");
+        private TextField price = new TextField("price");
+        private TextField quantity = new TextField("Quantity");
+        private TextField description = new TextField("Description");
+
+        private Binder<Product> binder = new Binder<>(Product.class);
+
+        public ProductForm() {
+            Button btnSave = new Button("Save", event -> {
+                shoppingService.updateProductSelective(binder.getBean());
+                updateList();
+                setProduct(null);
+            });
+
+            add(type, price, quantity, description, btnSave);
+            binder.bindInstanceFields(this);
+        }
+
+        private void setProduct(Product product) {
+            binder.setBean(product);
+            if (product == null) {
+                setVisible(false);
+            } else {
+                setVisible(true);
+                type.focus();
+            }
+        }
     }
 }
